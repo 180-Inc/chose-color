408595d58e941334a2e45acffadcda14b57ff76e
SHOGO180 <s.uenaka@180-inc.com>
2024-01-19 04:33:08 +0900

    update base

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  base:'chose-color',
+  base:'/chose-color/',
   // routes: [
   //   {
   //     path: '/',
