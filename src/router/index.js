5418c9ab89b08440ac1daf0fd0539c4637ac12c6
SHOGO180 <s.uenaka@180-inc.com>
2024-01-19 04:42:12 +0900

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import App from '../App.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   base:'/chose-color/',
-  // routes: [
-  //   {
-  //     path: '/',
-  //     name: 'home',
-  //     component: HomeView
-  //   },
-  //   {
-  //     path: '/about',
-  //     name: 'about',
-  //     // route level code-splitting
-  //     // this generates a separate chunk (About.[hash].js) for this route
-  //     // which is lazy-loaded when the route is visited.
-  //     component: () => import('../views/AboutView.vue')
-  //   }
-  // ]
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: App,
+    },
+  ]
 })
 
 export default router
